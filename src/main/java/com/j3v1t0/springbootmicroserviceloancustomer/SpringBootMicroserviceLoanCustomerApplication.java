c783e76f24c801cbb1b7305f7400dbce3ef484d7
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-01-24 09:05:33 -0400

    Loading initial project.

---
+++ src/main/java/com/j3v1t0/springbootmicroserviceloancustomer/SpringBootMicroserviceLoanCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.j3v1t0.springbootmicroserviceloancustomer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroserviceLoanCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceLoanCustomerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j3v1t0/springbootmicroserviceloancustomer/SpringBootMicroserviceLoanCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j3v1t0.springbootmicroserviceloancustomer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroserviceLoanCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
